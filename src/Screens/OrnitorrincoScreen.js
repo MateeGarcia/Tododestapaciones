45a0b72d73750a7272fe16c3526b93215b725203
MateeGarcia <51801253+MateeGarcia@users.noreply.github.com>
2019-09-12 23:23:56 -0300

    asd

--- src/Screens/OrnitorrincoScreen.js
+++ src/Screens/OrnitorrincoScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Navigator, TouchableHighlight} from 'react-native';
+import MapView from "react-native-maps";
 
 class OrnitorrincoScreen extends React.Component {
     render(){
         return(
             <Text>
-                ORNITORRINCO
+                <MapView>
+                    region={{
+                        latitude: 59.3293234999999,
+                        longitude: 18,
+                        longitudeDelta: 0.1,
+                        longitudeDelta: 0.1,
+                    }}
+                </MapView>
             </Text>     
         );
     }
